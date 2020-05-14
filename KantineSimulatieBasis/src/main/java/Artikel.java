87f453b8065e90df9b0275fc9f2385effc46cb0b
janine-dijkstra <janine00.dijkstra@gmail.com>
2020-05-14 13:50:35 +0200

    Artikel.java
    aanmaak Artikel.java

---
+++ KantineSimulatieBasis/src/main/java/Artikel.java
@@ -0,0 +1,40 @@
+public class Artikel{
+
+    //aanmaak velden
+    private String naam;
+    private int prijs;
+
+    //constructor met parameter C
+    public Atrikel(String naam, int prijs){
+        this.naam = naam;
+        this.prijs = prijs;
+    }
+
+    //constructor zonder parameter D
+    public Artikel(){
+        naam = "Naam";
+        prijs = 2;
+    }
+
+    //getters en setters E
+    public String getNaam(){
+        return naam;
+    }
+
+    public int getPrijs(){
+        return prijs
+    }
+
+    public void setNaam(String newNaam){
+        this.naam = newNaam
+    }
+
+    public void setPrijs(int newPrijs){
+        this.prijs = newPrijs
+    }
+
+}
+
+//Declaratie : Het opzetten van de variabele, het type en de naam van de variabele voor de eerste keer weergeven
+//Initialisatie: Een waarde geven aan een variabele.
+//Een variabele moet gedeclareerd zijn voordat deze geïnitialiseerd kan worden.
