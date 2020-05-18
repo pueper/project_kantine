c86d19dc4da23eb4fb06eee1ef3decc58002c4bb
janine-dijkstra <janine00.dijkstra@gmail.com>
2020-05-18 15:33:58 +0200

    verwijdering main
    main weggehaald uit dienblad, kassa bijna af

--- KantineSimulatieBasis/src/main/java/Dienblad.java
+++ KantineSimulatieBasis/src/main/java/Dienblad.java
@@ -4,71 +4,74 @@ import java.util.Stack;
 import java.util.HashMap;
 
 public class Dienblad {
-    public static void main(String[] args) {
-        private ArrayList<Artikel> artikelen;
-        private HashMap<Artikel, prijs> prijslijst;
+    private ArrayList<Artikel> artikelen;
+    private HashMap<Artikel, prijs> prijslijst;
 
-        /**
-         * Constructor zonder parameter
-         */
-        public Dienblad() {
-            ArrayList<Artikel> artikelen = new ArrayList<Artikel>();
-            HashMap<Artikel, prijs> prijslijst = new HashMap<Artikel, prijs>();
-        }
+    /**
+     * Constructor zonder parameter
+     */
+    public Dienblad() {
+        ArrayList<Artikel> artikelen = new ArrayList<Artikel>();
+        HashMap<Artikel, prijs> prijslijst = new HashMap<Artikel, prijs>();
+    }
 
-        //constructor met parameter klant (b)
-        public Dienblad(Persoon klant) {
-            ArrayList<Artikel> artikelen = new ArrayList<Artikel>();
-            HashMap<Artikel, prijs> prijslijst = new HashMap<Artikel, prijs>();
-            this.klant = klant
-        }
+    //constructor met parameter klant (b)
+    public Dienblad(Persoon klant) {
+        ArrayList<Artikel> artikelen = new ArrayList<Artikel>();
+        HashMap<Artikel, prijs> prijslijst = new HashMap<Artikel, prijs>();
+        this.klant = klant
+    }
 
-        //setter klant (b)
-        public void setKlant(Persoon klant){
-            this.klant = klant;
-        }
+    //setter klant (b)
+    public void setKlant(Persoon klant) {
+        this.klant = klant;
+    }
 
-        //getter klant (b)
-        public void getKlant(){
-            return klant;
-        }
+    //getter klant (b)
+    public void getKlant() {
+        return klant;
+    }
 
-        /**
-         * Methode om artikel aan dienblad toe te voegen
-         *
-         * @param artikel
-         */
-        public void voegToe (Artikel artikel){
-            artikelen.add(artikel);
+    /**
+     * Methode om artikel aan dienblad toe te voegen
+     *
+     * @param artikel
+     */
+    public void voegToe(Artikel artikel) {
+        artikelen.add(artikel);
 
-        }
+    }
 
-        /**
-         * Methode om aantal artikelen op dienblad te tellen
-         *
-         * @return Het aantal artikelen
-         */
-        public int getAantalArtikelen () {
-            aantal = artikelen.size();
-            return aantal;
-        }
+    /**
+     * Methode om aantal artikelen op dienblad te tellen
+     *
+     * @return Het aantal artikelen
+     */
+    public int getAantalArtikelen() {
+        aantal = artikelen.size();
+        return aantal;
+    }
 
-        /**
-         * Methode om de totaalprijs van de artikelen op dienblad uit te rekenen
-         *
-         * @return De totaalprijs
-         */
-        public double getTotaalPrijs () {
-            double totaal = 0;
-            if (artikel in artikelen) {
-                for (double i : prijslijst.values(); i > totaal; i++) {
-                    return totaal;
-                    }
-
-                }
+    /**
+     * Methode om de totaalprijs van de artikelen op dienblad uit te rekenen
+     *
+     * @return De totaalprijs
+     */
+    public double getTotaalPrijs() {
+        double totaal = 0;
+        if (artikel in artikelen){
+            for (double i : prijslijst.values() ;
+            i > totaal;
+            i++){
+                return totaal;
             }
+
         }
     }
+
 }
+
+
+
 
 
